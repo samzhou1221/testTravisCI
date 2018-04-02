51f3684ff77f70bfabcd871791d662d3d0b9e823
root <zhouxianxian@tj-fintech.com>
2018-04-02 14:54:55 +0800

    start a project from now

---
+++ common/package.go
@@ -0,0 +1,4 @@
+package common
+
+var lower int32
+var Upper int32
---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/testTravisCI/common"
+)
+
+func main() {
+	fmt.Println("This is a test.")
+	fmt.Println(common.Upper)
+	//fmt.Println(common.lower) // wrong usage
+
+}
